848de86695b2dcd5e673ae6c002494e53329505e
Enock Omondi <enockomondi305@gmail.com>
2026-07-14 08:17:54 +0300

    Add: two-integer-sum - submission-10

---
+++ Data Structures & Algorithms/two-integer-sum/submission-10.js
@@ -0,0 +1,62 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums, target) {
+        // BRUTE FORCE SOLUTION
+        // for(let i =0; i<nums.length; i++){
+        //     for(let j = i + 1; j<nums.length; j++){
+        //         let sum = nums[i] + nums[j]
+        //         if(sum == target){
+        //             return [i, j]
+        //         }
+        //     }
+        // }
+
+        // OPTIMIZED SOLUTION: MULTIPLE POINTERS
+        // first copy the nums array to another array
+        // const newNums = [...nums]
+        // // sort the new array
+        // newNums.sort((a, b) =>  a-b)
+
+        // // declare the pointers
+        // let start = 0
+        // let end = nums.length - 1
+        // // condition
+        // while(start < end){
+        //     let sum = newNums[start] + newNums[end]
+        //     if(sum == target){
+        //         // define the indices
+        //         let startIndex = nums.indexOf(newNums[start])
+        //         let endIndex = nums.lastIndexOf(newNums[end])
+        //         return [startIndex, endIndex]
+        //     }else if(sum > target){
+        //         end--
+        //     }else{
+        //         start++
+        //     }
+        // }
+        // return [-1, -1]
+
+
+        // HASH MAP: 0(n)
+
+        // create a new map
+        let numsMap = new Map()
+
+        // loop through nums array and check the items in the map
+        for(let i =0; i<nums.length; i++){
+            let complement = target - nums[i]
+
+            // check if it already exist in the map and return
+            if(numsMap.has(complement)){
+                return [numsMap.get(complement), i]
+            }
+            numsMap.set(nums[i], i)
+        }
+
+        return [-1, -1]
+    }
+}
